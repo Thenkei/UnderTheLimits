d6315f51a4647572027ae55a1f08653007e6af30
Arnaud <arnaud.moncel43@gmail.com>
2018-12-13 19:39:46 +0100

    Adding db connection & populate
    Add npm run populate function(database need up before)
    Add sql & js scripts for populate ./server/scripts
    Add docker-compose file run mysql database & phpmyadmin
    Move server file in src folder
    Add models folder for database models

--- server/index.js
+++ server/index.js
@@ -1,24 +1,39 @@
-const io = require( 'socket.io' )();
-const Channel = require( './channel' );
-const Player = require( './player' );
+(async () => {
+  const io = require( 'socket.io' )();
+  const Channel = require( './src/channel' );
+  const Player = require( './src/player' );
+  
+  try {
+    const config = {
+      db: 'utl',
+      db_user: 'user',
+      db_pwd: 'pwd',
+      db_host: 'localhost',
+      db_log: false
+    };
+    const db = await require('./src/models')(config);
 
+  } catch (err) {
+    console.error(err);
+  }
 
-let TEST_CHANNEL = new Channel( [ new Player( 'aaa' ), new Player( 'bbb' ), new Player( 'ccc' ) ] );
-TEST_CHANNEL.addPlayer( new Player( 'ddd' ) );
-console.log(TEST_CHANNEL.getPlayersCount());
-TEST_CHANNEL.removePlayerByName( 'aaa' );
-console.log(TEST_CHANNEL.getPlayersCount());
-TEST_CHANNEL.addPlayer( new Player( 'eee' ) );
-TEST_CHANNEL.listPlayers();
-io.on( 'connection', ( client ) => {
-    client.on( 'subscribeToTimer', ( interval ) => {
-        console.log( 'client is subscribing to timer with interval ', interval );
-        setInterval( () => {
-            client.emit( 'timer', new Date() );
-        }, interval );
-    } );
-} );
+  let TEST_CHANNEL = new Channel( [ new Player( 'aaa' ), new Player( 'bbb' ), new Player( 'ccc' ) ] );
+  TEST_CHANNEL.addPlayer( new Player( 'ddd' ) );
+  console.log(TEST_CHANNEL.getPlayersCount());
+  TEST_CHANNEL.removePlayerByName( 'aaa' );
+  console.log(TEST_CHANNEL.getPlayersCount());
+  TEST_CHANNEL.addPlayer( new Player( 'eee' ) );
+  TEST_CHANNEL.listPlayers();
+  io.on( 'connection', ( client ) => {
+      client.on( 'subscribeToTimer', ( interval ) => {
+          console.log( 'client is subscribing to timer with interval ', interval );
+          setInterval( () => {
+              client.emit( 'timer', new Date() );
+          }, interval );
+      } );
+  } );
 
-const port = 3001;
-io.listen( port );
-console.log( 'listening on port ', port );
+  const port = 3001;
+  io.listen( port );
+  console.log( 'listening on port ', port );
+})();
