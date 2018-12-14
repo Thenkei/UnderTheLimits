570c2cf8c439e211068748100249316ef280762a
Jeff LADIRAY <ladirayjeff@gmail.com>
2018-12-14 23:35:22 +0100

    Just to avoid bad word ref :)
    Just ask for password if needed

---
+++ server/scripts/postinstall.sh
@@ -0,0 +1,2 @@
+./node_modules/.bin/nodecipher decrypt server/scripts/populate_question.sql.enc server/scripts/populate_question.sql
+./node_modules/.bin/nodecipher decrypt server/scripts/populate_answer.sql.enc server/scripts/populate_answer.sql
