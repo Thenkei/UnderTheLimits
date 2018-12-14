daa7124b15c7b16d046d0d5fd7a5d0dc11739448
Jeff LADIRAY <ladirayjeff@gmail.com>
2018-12-14 15:30:13 +0100

    Linter should now be active on the projet

--- server/index.js
+++ server/index.js
@@ -1,40 +1,43 @@
-(async () => {
-  const io = require( 'socket.io' )();
-  const Channel = require( './src/channel' );
-  const Player = require( './src/player' );
+
+async function start() {
+  const io = require('socket.io')(); // eslint-disable-line global-require
+  const Channel = require('./src/channel'); // eslint-disable-line global-require
+  const Player = require('./src/player'); // eslint-disable-line global-require
   try {
     const config = {
       db: 'utl',
       db_user: 'user',
       db_pwd: 'pwd',
       db_host: '0.0.0.0',
-      db_log: false
+      db_log: false,
     };
 
-    const db = await require('./src/models')(config);
-    let TEST_CHANNEL = new Channel( [ new Player( 'aaa' ), new Player( 'bbb' ), new Player( 'ccc' ) ] );
-    TEST_CHANNEL.addPlayer( new Player( 'ddd' ) );
-    console.log(TEST_CHANNEL.getPlayersCount());
-    TEST_CHANNEL.removePlayerByName( 'aaa' );
-    console.log(TEST_CHANNEL.getPlayersCount());
-    TEST_CHANNEL.addPlayer( new Player( 'eee' ) );
+    await require('./src/models')(config); // eslint-disable-line global-require
+    const TEST_CHANNEL = new Channel([new Player('aaa'), new Player('bbb'), new Player('ccc')]);
+    TEST_CHANNEL.addPlayer(new Player('ddd'));
+    TEST_CHANNEL.removePlayerByName('aaa');
+    TEST_CHANNEL.addPlayer(new Player('eee'));
     TEST_CHANNEL.listPlayers();
-    io.on( 'connection', ( client ) => {
-        client.on( 'subscribeToTimer', ( interval ) => {
-            console.log( 'client is subscribing to timer with interval ', interval );
-            setInterval( () => {
-                client.emit( 'timer', new Date() );
-            }, interval );
-        } );
-        client.on( 'createPlayer', ( playerName ) => {
-          console.warn( 'createPlayer ' + playerName );
-            client.emit( 'playerCreated', new Player( playerName ) );
-        } );
-    } );
+    io.on('connection', (client) => {
+      client.on('subscribeToTimer', (interval) => {
+        console.warn('client is subscribing to timer with interval ', interval);
+        setInterval(() => {
+          client.emit('timer', new Date());
+        }, interval);
+      });
+      client.on('createPlayer', (playerName) => {
+        console.warn(`createPlayer ${playerName}`);
+        client.emit('playerCreated', new Player(playerName));
+      });
+    });
   } catch (err) {
     console.error(err);
   }
   const port = 3001;
-  io.listen( port );
-  console.log( 'listening on port ', port );
+  io.listen(port);
+  console.warn('listening on port ', port);
+}
+
+(async () => {
+  start();
 })();
