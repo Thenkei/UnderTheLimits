0f32a20406fa351f90dc00e815ed59dfd0081f26
Thenkei <morganperre@gmail.com>
2019-01-24 00:14:38 +0100

    Random player order for judgment
    + close #30

--- server/src/channel.js
+++ server/src/channel.js
@@ -94,6 +94,12 @@ class Channel {
   //----
   nextRound() {
     this.initializePlayersCards();
+
+    for (let i = this.players.length - 1; i > 0; i -= 1) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.players[i], this.players[j]] = [this.players[j], this.players[i]];
+    }
+
     this.currentStatus = CHANNEL_STATUS.PLAYING_CARD;
   }
 
