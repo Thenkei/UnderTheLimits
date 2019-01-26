20b5e9f9dab262910425ace7b169d924b4754b9f
Jeff LADIRAY <ladirayjeff@gmail.com>
2019-01-26 21:25:34 +0100

    Initial client commit

---
+++ precache-manifest.c3f4ed953285059d74acddfbb1b3612d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bef8dfacd800360b225b",
+    "url": "/static/js/main.bef8dfac.chunk.js"
+  },
+  {
+    "revision": "0db32c6b045807a2ca14",
+    "url": "/static/js/1.0db32c6b.chunk.js"
+  },
+  {
+    "revision": "bef8dfacd800360b225b",
+    "url": "/static/css/main.254dbc21.chunk.css"
+  },
+  {
+    "revision": "d9a11609add847d1c7be588592cc78e3",
+    "url": "/index.html"
+  }
+];
