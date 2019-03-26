c38f7481738ace4b82250eed3ddc9a7a9571ddb3
Jeff LADIRAY <ladirayjeff@gmail.com>
2019-03-26 21:52:47 +0100

    Initial test for deployment
    Initial test for deployment
    
    Second try
    
    third try
    
    4th try
    
    5th try - Fix env var
    
    Final try ?

---
+++ server/scripts/populate_question_dev.sql
@@ -0,0 +1,11 @@
+INSERT INTO `Questions` (`id`, `text`) VALUES
+(1, 'Grâce aux tutos vidéos de ______ j’ai enfin pu ______.'),
+(2, 'L’autre jour ma grand-mère m’a proposé ______, un véritable enchantement pour moi.'),
+(3, 'À mon grand regret, les nazis n’ont pas réussi à ______.'),
+(4, 'Aucun mérite pour ______. ______ était vraiment trop facile.'),
+(5, 'Tu as déjà été obligé de ______ pour faire plaisir à ta mère ?'),
+(6, 'L’autre jour j’ai donné ______ au Téléthon. Ça devrait leur être utile.'),
+(7, 'Je ne supporte plus de payer pour ______.'),
+(8, 'Papa ? Quand est-ce qu’on achète ______ ? Maman me l’avait promis !'),
+(9, 'Ma Banque m’a appelé Mardi. Ma conseillère a décidé de ______. Je suis ravi.'),
+(10, 'Allons enfants de la Patrie, ______ est arrivé.');
