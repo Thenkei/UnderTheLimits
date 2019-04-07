4dd1a30605e4e69644b9c52630bd7f2520e63d83
Arnaud <arnaud.moncel43@gmail.com>
2019-04-07 18:01:20 +0200

    Add mute sound button on TopAppBar

--- client/src/components/TopAppBar/index.jsx
+++ client/src/components/TopAppBar/index.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import './style.scss';
+import { IconButton, Icon } from '..';
 
-const TopAppBar = () => (
+import { toggleSound } from '../../reducers/app';
+
+const TopAppBar = props => (
   <header className='TopAppBar'>
     <img
       src='/public/images/UTL_Logo.png'
       alt='under-the-limits'
       className='TopAppBar-logo'
     />
+    <IconButton onClick={props.toggleSound}>
+      <Icon>{props.isSoundMuted ? 'volume_up' : 'volume_off'}</Icon>
+    </IconButton>
   </header>
 );
 
-export default TopAppBar;
+TopAppBar.propTypes = {
+  isSoundMuted: PropTypes.bool.isRequired,
+  toggleSound: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => ({
+  isSoundMuted: state.app.isSoundMuted,
+});
+
+const mapDispatchToProps = dispatch => ({
+  toggleSound: () => dispatch(toggleSound()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TopAppBar);
