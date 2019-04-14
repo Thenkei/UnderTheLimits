e83dd62f834bbdd09b28890a3499e7ae698e2b65
Jeff LADIRAY <ladirayjeff@gmail.com>
2019-04-14 13:23:04 +0200

    Fix lint issues

--- client/src/components/Chat/Message.jsx
+++ client/src/components/Chat/Message.jsx
@@ -16,7 +16,7 @@ const Message = ({
   return (
     <div className={classes.wrapper}>
       <Avatar className={classes.avatar} src={`${window.location.origin}/avatars/${avatar}.png`} />
-      <div className={classes.sendedMessage}> 
+      <div className={classes.sendedMessage}>
         <Typography variant='h6' className={classes.playerName} align='left'>{username}</Typography>
         <Typography className={classes.text}>{decodeURI(message)}</Typography>
         <Typography variant='caption' className={classes.metaDate} align='left'>{formattedDate}</Typography>
