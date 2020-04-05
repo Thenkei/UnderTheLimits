9d7cd78ebcb93114381eeb0cfa9db62a450ccbe4
Jeff LADIRAY <ladirayjeff@gmail.com>
2020-04-05 21:20:02 +0200

    Shoudl finally fix circleci

--- server/createDBConfig.js
+++ server/createDBConfig.js
@@ -1,13 +1,5 @@
 
-module.exports = (env = process.env.NODE_ENV) => (env === 'production' ? {
-  db: process.env.DB || 'utl',
-  user: process.env.DB_USER || 'user',
-  pwd: process.env.DB_PWD || 'pwd',
-  host: process.env.DB_HOST || 'mysql',
-  port: process.env.DB_PORT || 3306,
-  dialect: process.env.DB_DIALECT || 'mysql',
-  log: () => {},
-} : {
+module.exports = () => ({
   dialect: process.env.DB_DIALECT || 'sqlite',
   storage: process.env.DB_STORAGE || `${__dirname}/db.sqlite3`,
   log: () => {},
